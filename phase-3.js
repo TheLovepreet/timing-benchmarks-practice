f9cb69208b8d88631c5c7a4994812b3b6271bdec
TheLove <lovepreetsinghcad@gmail.com>
2022-12-06 12:33:11 -0500

    Time Complexity for functions

--- phase-3.js
+++ phase-3.js
@@ -2,15 +2,37 @@ const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
   // Copy your `addNums10` code here
+    // Fill this in
+    let total = 0;
+    let answer = [];
+    console.log(Date.now());
+    for(let i = increment; i <= increment * 10; i = i + increment){
+      total =  addNums(i);
+      console.log(Date.now());
+      answer.push(total);
+    }
+    return answer;
+  }
   // Then, add timing code
-
   // Your code here
 
-}
+
 
 
 function addManyNums10Timing(increment) {
 // Copy your `addManyNums10` code here
+  // Fill this in
+  let total = 0;
+  let answer = [];
+  console.log(Date.now());
+  for(let i = increment; i <= increment*10; i = i + increment ){
+    total = addManyNums(i);
+    console.log(Date.now());
+    answer.push(total);
+  }
+  return  answer;
+
+
 // Then, add timing code
 
   // Your code here
